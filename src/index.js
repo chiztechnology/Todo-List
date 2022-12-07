c9dd8bf7dc8f33f9469b1136bba447e61120aebd
Israel CHIZUNGU <chizunguisrael@gmail.com>
2022-12-08 00:17:47 +0200

    Fix linters

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './styles.css';
 // import ResyncIcon from './resync-icon.png';
 import OptionIcon from './OptionIcon.png';
 
-let tasks = [
+const tasks = [
   {
     index: 1,
     description: 'Read my lesson',
@@ -23,10 +23,9 @@ let tasks = [
 // document.querySelector('.resync-icon').src = ResyncIcon;
 // document.querySelectorAll('.option-icon').src = OptionIcon;
 
-
-window.onload = ()=>{
+window.onload = () => {
   // fetch tasks
-  tasks.forEach((task)=>{
+  tasks.forEach((task) => {
     document.querySelector('.listTodo').innerHTML += `<article class="todo-article">
     <div>
       <input type="checkbox" name="check" id="checkbox-${task.index}" value="">
@@ -35,16 +34,21 @@ window.onload = ()=>{
     <a href="#"><img src=${OptionIcon} alt="option icon" class="option-icon"></a>
   </article>`;
   });
-
 };
 
-document.getElementById('todo-form').addEventListener('submit', (e)=>{
+document.getElementById('todo-form').addEventListener('submit', (e) => {
   e.preventDefault();
-  let desc = document.getElementById('todo-desc').value;
-  tasks.push(new Object({index: tasks[tasks.length-1].index + 1, description: desc, completed: false}));
+  const desc = document.getElementById('todo-desc').value;
+  const obj = new Object(
+    {
+      index: tasks[tasks.length - 1].index + 1,
+      description: desc,
+      completed: false
+    })
+  tasks.push(obj);
   // refresh DOM by adding new value on the screen
 })
 
-document.getElementById('clear-completed').addEventListener('click', (e)=>{
+document.getElementById('clear-completed').addEventListener('click', (e) => {
   e.preventDefault();
 })
