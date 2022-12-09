23ba23fcb0c4bb2a353414b4a9492f152f27a64e
Israel CHIZUNGU <chizunguisrael@gmail.com>
2022-12-09 19:42:15 +0200

    Fix linters

--- src/modules/clear-completed-task.js
+++ src/modules/clear-completed-task.js
@@ -3,7 +3,6 @@ import resetIndex from './index-resetter.js';
 export default function clearCompleted(tasks) {
   const tempArray = [];
   // get completed tasks indexes
-  // eslint-disable-next-line no-use-before-define
   for (let i = 0; i < tasks.length; i++) {
     if (tasks[i].completed) {
       tempArray.push(tasks[i].index);
@@ -11,7 +10,6 @@ export default function clearCompleted(tasks) {
   }
 
   // delete values
-  // eslint-disable-next-line no-use-before-define
   for (let i = 0; i < tempArray.length; i++) {
     tasks = tasks.filter((element) => element.index !== tempArray[i]);
   }
--- src/modules/index-resetter.js
+++ src/modules/index-resetter.js
@@ -1,5 +1,4 @@
 export default function resetIndex(array) {
-  // eslint-disable-next-line no-use-before-define
   for (let i = 1; i <= array.length; i++) {
     array[i - 1].index = i;
   }
