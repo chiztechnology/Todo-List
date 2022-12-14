504400290dc585f08884386cf641ff2b8c90c253
Israel CHIZUNGU <chizunguisrael@gmail.com>
2022-12-15 00:16:07 +0200

    Clear code

--- tasks.test.js
+++ tasks.test.js
@@ -1,36 +1,35 @@
-import { addTask, removeTask } from "./src/modules/tasks";
+import { addTask, removeTask } from './src/modules/tasks.js';
 
-jest.mock("./src/modules/Save-and-load.js");
+jest.mock('./src/modules/Save-and-load.js');
 
-describe("Add and remove Tasks", () => {
-  it("Add task", () => {
+describe('Add and remove Tasks', () => {
+  it('Add task', () => {
     expect(
       addTask(
         {
           index: 32,
-          description: "Paying water bills",
+          description: 'Paying water bills',
           completed: false,
         },
-        []
-      )
+        []),
     ).toStrictEqual([
       {
         index: 32,
-        description: "Paying water bills",
+        description: 'Paying water bills',
         completed: false,
       },
     ]);
   });
 
-  it("remove Task", () => {
+  it('remove Task', () => {
     expect(
       removeTask(32, [
         {
           index: 32,
-          description: "Paying water bills",
+          description: 'Paying water bills',
           completed: false,
         },
-      ])
+      ]),
     ).toStrictEqual([]);
   });
 });
