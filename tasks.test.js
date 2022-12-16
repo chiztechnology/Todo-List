15245b5db8ae4ed7fe8869467b80e63fbcb036fd
iqbal-elham <iqbal.elham7@gmail.com>
2022-12-16 22:56:07 +0430

    Update the tests using local storage mock

--- tasks.test.js
+++ tasks.test.js
@@ -5,73 +5,43 @@ import {
   updateStatus,
 } from './src/modules/tasks.js';
 
-jest.mock('./src/modules/Save-and-load.js');
+import 'jest-localstorage-mock';
+
+const task1 = {
+  index: 0,
+  description: 'Pay the bills',
+  completed: true,
+};
 
 describe('Add and remove Tasks', () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
   it('Add task', () => {
-    expect(
-      addTask(
-        {
-          index: 32,
-          description: 'Paying water bills',
-          completed: false,
-        },
-        [],
-      ),
-    ).toStrictEqual([
-      {
-        index: 32,
-        description: 'Paying water bills',
-        completed: false,
-      },
-    ]);
+    addTask(task1, (localStorage.getItem('todo-list') || []));
+    const storage = JSON.parse(localStorage.getItem('todo-list'));
+    expect(storage[0].index).toBe(0);
   });
 
   it('remove Task', () => {
+    addTask(task1, (localStorage.getItem('todo-list') || []));
     expect(
-      removeTask(32, [
-        {
-          index: 32,
-          description: 'Paying water bills',
-          completed: false,
-        },
-      ]),
+      removeTask(0, JSON.parse(localStorage.getItem('todo-list'))),
     ).toStrictEqual([]);
   });
 
   it('Update the task', () => {
-    expect(
-      updateTask('Pay the bills', 0, [
-        {
-          index: 0,
-          description: 'Paying water bills',
-          completed: false,
-        },
-      ]),
-    ).toStrictEqual([
-      {
-        index: 0,
-        description: 'Pay the bills',
-        completed: false,
-      },
-    ]);
+    addTask(task1, (localStorage.getItem('todo-list') || []));
+    const storage = JSON.parse(localStorage.getItem('todo-list'));
+    updateTask('Pay the bills', 0, storage);
+    expect(storage[0].description).toStrictEqual('Pay the bills');
   });
 
   it('Update complete status', () => {
-    expect(
-      updateStatus(false, 0, [
-        {
-          index: 0,
-          description: 'Paying water bills',
-          completed: false,
-        },
-      ]),
-    ).toStrictEqual([
-      {
-        index: 0,
-        description: 'Paying water bills',
-        completed: true,
-      },
-    ]);
+    addTask(task1, (localStorage.getItem('todo-list') || []));
+    const storage = JSON.parse(localStorage.getItem('todo-list'));
+    updateStatus(true, 0, storage);
+    expect(storage[0].completed).toStrictEqual(false);
   });
 });
